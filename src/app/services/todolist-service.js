8cd2977df9462fa1368cff9778e7243eb7fc16d0
Volkov-Illya <Armiavolkov@gmail.com>
2016-11-15 21:45:20 +0200

    saving temporary changes

--- src/app/services/todolist-service.js
+++ src/app/services/todolist-service.js
@@ -7,9 +7,16 @@
 
   function elTodoListService(Restangular) {
 
+    function isFavourite(task) {
+      return Restangular.one('todo/isFavourite', task._id).customPUT(task)
+    }
+
+    function isDone(task) {
+      return Restangular.one('todo/isDone', task._id).customPUT(task)
+    }
+
     function getAll() {
       return Restangular.all('todo').getList();
-
     }
 
     function create(data) {
@@ -25,15 +32,55 @@
     }
 
     function edit(task) {
-      return Restangular.all('todo').one(task._id).customPUT(task);
+      return Restangular.one('todo', task._id).customPUT(task)
+        .then(function () {
+          task.name = null;
+        })
     }
+
+    function findOne(task) {
+    }
+
+
+    function createSubtask(task) {
+      return Restangular.one('todo/subtask/', task._id).customPOST({name: task.subTask.name})
+
+    }
+
+    function removeSub(task, sub) {
+      return Restangular.one('todo/subtask/', task._id).customDELETE(sub.name);
+    }
+
+    function getAllSubtask(task) {
+      console.log(task);
+      return Restangular.one('todo/subtask', task._id).getList();
+    }
+
+    function isSubDone(task, sub) {
+      return Restangular.one('todo/subtask/', task._id).customPUT({name: sub.name});
+    }
+
+    function editSub(task, sub) {
+      console.log(task);
+      console.log(sub.name);
+      return Restangular.one('todo/subtask', task._id).customPUT(sub.name)
+    }
+
+
 
     return {
       getAll: getAll,
       create: create,
       remove: remove,
-      edit: edit
-
+      edit: edit,
+      isDone: isDone,
+      isFavourite: isFavourite,
+      findOne: findOne,
+      createSubtask: createSubtask,
+      getAllSubtask: getAllSubtask,
+      isSubDone: isSubDone,
+      removeSub:removeSub,
+      editSub:editSub
     }
   }
 })();
--- src/app/todolist/toDoListCtrl.js
+++ src/app/todolist/toDoListCtrl.js
@@ -1,26 +1,59 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('todoListUi')
-        .controller('TodoListCtrl', TodoListCtrl);
+  angular
+    .module('todoListUi')
+    .controller('TodoListCtrl', TodoListCtrl);
 
-    /** @ngInject */
-    function TodoListCtrl(elTodoListService) {
+  /** @ngInject */
+  function TodoListCtrl(elTodoListService) {
 
-        var vm = this;
-        vm.getAll = getAll;
-        vm.addTask = addTask;
+    var vm = this;
+    vm.getAll = getAll();
+    vm.addTask = addTask;
+    vm.remove = remove;
+    vm.edit = edit;
+    vm.isDone = isDone;
+    vm.isFavourite = isFavourite;
+    vm.meow = meow;
+    vm.findOne = findOne;
+    vm.createSubtask = createSubtask;
+    vm.getAllSubtask = getAllSubtask;
+    vm.isSubDone = isSubDone;
+    vm.removeSub = removeSub;
+    vm.editSub = editSub;
 
-        function addTask(data) {
-            elTodoListService.createTask(data)
-        }
 
-        function getAll() {
-            elTodoListService.getAll().then(function (result) {
-                vm.getAll = result;
-            });
-        }
+
+    function meow() {
+      alert('MEOW BLYJAT')
+    }
+
+    function findOne(task) {
+      elTodoListService.findOne(task)
+    }
+
+    function isFavourite(task) {
+      elTodoListService.isFavourite(task)
+        .then(function () {
+          return getAll();
+        });
+    }
+
+    function isDone(task) {
+      elTodoListService.isDone(task)
+        .then(function () {
+          return getAll();
+        });
+    }
+
+
+    function getAll() {
+      elTodoListService.getAll()
+        .then(function (result) {
+          vm.getAll = result;
+        });
+    }
 
     function addTask(task) {
       elTodoListService.create(task)
@@ -28,6 +61,7 @@
           return getAll();
         });
     }
+
 
     function remove(id) {
       elTodoListService.remove(id)
@@ -41,6 +75,44 @@
         .then(function () {
           return getAll();
         })
+    }
+
+
+    function getAllSubtask(task) {
+      console.log(task);
+      elTodoListService.getAllSubtask(task)
+        .then(function (result) {
+         return  result;
+        });
+    }
+
+    function createSubtask(task) {
+      elTodoListService.createSubtask(task)
+        .then(function (task) {
+          console.log('create');
+          return getAllSubtask(task);
+        })
+    }
+
+    function isSubDone(task, sub) {
+      elTodoListService.isSubDone(task, sub)
+        .then(function (task) {
+          return getAllSubtask(task);
+        });
+    }
+
+    function removeSub(task, sub) {
+      elTodoListService.removeSub(task, sub)
+        .then(function (task) {
+          console.log(task._id);
+          return getAllSubtask(task);
+        })
+    }
+
+    function editSub(task,sub) {
+      elTodoListService.editSub(task, sub)
+
+
     }
 
   }
