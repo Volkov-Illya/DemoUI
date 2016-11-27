706da573d2d7244e3435762949ecaa950eb753ae
Volkov-Illya <Armiavolkov@gmail.com>
2016-11-27 18:43:44 +0200

    preprod

--- gulp/constants.js
+++ gulp/constants.js
@@ -1,23 +1,25 @@
-var gulp = require('gulp');
+var gulp       = require('gulp');
 var ngConstant = require('gulp-ng-constant');
 
 gulp.task('constants:dev', function () {
-  var myConfig = require('../src/config.json');
+  var myConfig  = require('../src/config.json');
   var envConfig = myConfig['development'];
   return ngConstant({
-      constants: envConfig,
-      stream: true
-    })
+    constants: envConfig,
+    stream   : true,
+    wrap     : false
+  })
     .pipe(gulp.dest('src/app/constants'));
 });
 
 gulp.task('constants:stage', function () {
-  var myConfig = require('../src/config.json');
+  var myConfig  = require('../src/config.json');
   var envConfig = myConfig['staging'];
   return ngConstant({
-      constants: envConfig,
-      stream: true
-    })
+    constants: envConfig,
+    stream   : true,
+    wrap     : false
+  })
     .pipe(gulp.dest('src/app/constants'));
 });
 
